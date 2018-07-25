79ae508465f2ff2ac7ea02ae691c93a8d1222d7f
Shakib Hossain <shakib609@gmail.com>
2018-07-25 19:33:16 +0600

    Solve A2OJ 1

---
+++ A2/1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+main()
+{
+    unsigned int n, x, y;
+    while (n--) {
+        cin >> x >> y;
+        cout << x + y << endl;
+    }
+    return 0;
+}
