294f57f5ea96f310ba1095a0ae2d709c2c7965f3
Shakib Hossain <shakib609@gmail.com>
2022-05-30 04:34:46 +0600

    Add Go Lang solution for 485 problem - leetcode

--- LeetCode/000001.go
+++ LeetCode/000001.go
@@ -1,35 +1,37 @@
+package twoSum
+
 func twoSum_n2(nums []int, target int) []int {
-    totalNums := len(nums)
-    result := make([]int, 2)
-    for i := 0; i < totalNums; i++ {
-        for j := i + 1; j < totalNums; j++ {
-            if nums[i] + nums[j] == target {
-                result[0] = i
-                result[1] = j
-                return result
-            }
-        }
-    }
-    return result
+	totalNums := len(nums)
+	result := make([]int, 2)
+	for i := 0; i < totalNums; i++ {
+		for j := i + 1; j < totalNums; j++ {
+			if nums[i]+nums[j] == target {
+				result[0] = i
+				result[1] = j
+				return result
+			}
+		}
+	}
+	return result
 }
 
 func twoSum_n(nums []int, target int) []int {
-    totalNums := len(nums)
-    hashmap := map[int]int{}
-    result := make([]int, 2)
-    for i := 0; i < totalNums; i++ {
-        complement := target - nums[i]
-        if _, exists := hashmap[complement]; exists {
-            result[0] = i
-            result[1] = hashmap[complement]
-            break
-        } else {
-            hashmap[nums[i]] = i
-        }
-    }
-    return result
+	totalNums := len(nums)
+	hashmap := map[int]int{}
+	result := make([]int, 2)
+	for i := 0; i < totalNums; i++ {
+		complement := target - nums[i]
+		if _, exists := hashmap[complement]; exists {
+			result[0] = i
+			result[1] = hashmap[complement]
+			break
+		} else {
+			hashmap[nums[i]] = i
+		}
+	}
+	return result
 }
 
 func twoSum(nums []int, target int) []int {
-    return twoSum_n(nums, target)
+	return twoSum_n(nums, target)
 }
---
+++ LeetCode/000485.go
@@ -0,0 +1,18 @@
+package findMaxConsecutiveOnes
+
+func findMaxConsecutiveOnes(nums []int) int {
+	result := 0
+	temp_sum := 0
+
+	for _, v := range nums {
+		if v == 0 {
+			temp_sum = 0
+		} else {
+			temp_sum += 1
+			if temp_sum > result {
+				result = temp_sum
+			}
+		}
+	}
+	return result
+}
