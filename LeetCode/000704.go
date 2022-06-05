1abd13f3a69c97b7e38a39f0e0f9dbca405303f7
Shakib Hossain <shakib609@gmail.com>
2022-06-06 01:44:17 +0600

    Solve LeetCode - 704 - Binary Search

---
+++ LeetCode/000704.go
@@ -0,0 +1,17 @@
+package leetcode
+
+// O(logn)
+func search(nums []int, target int) int {
+	i, j := 0, len(nums)-1
+	for i <= j {
+		mid := (i + j) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			j = mid - 1
+		} else {
+			i = mid + 1
+		}
+	}
+	return -1
+}
