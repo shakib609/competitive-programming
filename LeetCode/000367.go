9dfd25b6b1b92791f9f3efc71a730eb8105cbbe0
Shakib Hossain <shakib609@gmail.com>
2022-06-19 21:08:53 +0600

    Solve LeetCode - 367 - Valid Perfect Square using Binary search

--- LeetCode/000367.go
+++ LeetCode/000367.go
@@ -1,6 +1,6 @@
 package leetcode
 
-func isPerfectSquare(num int) bool {
+func isPerfectSquareLoop(num int) bool {
 	for i := 0; ; i++ {
 		squaredValue := i * i
 		if squaredValue == num {
@@ -10,4 +10,23 @@ func isPerfectSquare(num int) bool {
 		}
 	}
 	return false
+}
+
+func isPerfectSquareBinarySearch(num int) bool {
+	start, end := 0, num
+	for start <= end {
+		guess := (start + end) / 2
+		squaredValue := guess * guess
+		if squaredValue == num {
+			return true
+		} else if squaredValue > num {
+			end = guess - 1
+		} else if squaredValue < num {
+			start = guess + 1
+		}
+	}
+	return false
+}
+func isPerfectSquare(num int) bool {
+	return isPerfectSquareBinarySearch(num)
 }
